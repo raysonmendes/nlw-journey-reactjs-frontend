3fd3417f732c736e673dd3f4c77c145a57b916d0
Rayson Mendes <raysonmendes2.0@gmail.com>
2024-07-13 12:15:48 -0300

    feat (creat-trip): creat a page for creat trips

---
+++ src/pages/create-trip/index.tsx
@@ -0,0 +1,137 @@
+import React, { FormEvent, useState } from "react";
+
+import LogoIcon from "../../../components/logo";
+import { useNavigate } from "react-router-dom";
+import { InviteGuestsModal } from "./invite-guests-modal";
+import { ConfirmTripModal } from "./confirm-trip-modal";
+import { DestinationAndDateStep } from "./steps/destination-and-date";
+import { InviteGuestsStep } from "./steps/invite-guests";
+
+function CreateTripPage() {
+  const navigate = useNavigate();
+
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState<boolean>(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState<boolean>(false);
+  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([]);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] =
+    useState<boolean>(false);
+
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true);
+  }
+
+  function closeGuestsInput() {
+    setIsGuestsInputOpen(false);
+  }
+
+  function openGuestsModal() {
+    setIsGuestsModalOpen(true);
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false);
+  }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true);
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false);
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email")?.toString();
+
+    if (!email) {
+      return;
+    }
+
+    if (emailsToInvite.includes(email)) {
+      alert(`${email}, já está na lista!`);
+
+      event.currentTarget.reset();
+      return;
+    }
+
+    setEmailsToInvite((prev) => [...prev, email]);
+
+    event.currentTarget.reset();
+  }
+
+  function removeEmailFromInvites(emailToRemove: string) {
+    const newEmailsList = emailsToInvite.filter(
+      (email) => email !== emailToRemove
+    );
+
+    setEmailsToInvite(newEmailsList);
+  }
+
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    navigate("/trip/123");
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="  max-w-3xl w-full px-6 text-center  space-y-10">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <LogoIcon />
+          <p className="text-zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+
+        <div className="space-y-4">
+          <DestinationAndDateStep
+            isGuestsInputOpen={isGuestsInputOpen}
+            openGuestsInput={openGuestsInput}
+            closeGuestsInput={closeGuestsInput}
+          />
+
+          {isGuestsInputOpen && (
+            <InviteGuestsStep
+              emailsToInvite={emailsToInvite}
+              openConfirmTripModal={openConfirmTripModal}
+              openGuestsModal={openGuestsModal}
+            />
+          )}
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
+          <br /> com nossos{" "}
+          <a className="text-zinc-300 underline" href="#">
+            termos de uso
+          </a>{" "}
+          e{" "}
+          <a className="text-zinc-300 underline" href="#">
+            {" "}
+            políticas de privacidade.
+          </a>
+        </p>
+      </div>
+
+      {isGuestsModalOpen && (
+        <InviteGuestsModal
+          closeGuestsModal={closeGuestsModal}
+          emailsToInvite={emailsToInvite}
+          addNewEmailToInvite={addNewEmailToInvite}
+          removeEmailFromInvites={removeEmailFromInvites}
+        />
+      )}
+
+      {isConfirmTripModalOpen && (
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
+      )}
+    </div>
+  );
+}
+
+export default CreateTripPage;
