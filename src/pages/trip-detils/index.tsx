278720d94806415501eade4a9c9879ccddb4785e
Rayson Mendes <raysonmendes2.0@gmail.com>
2024-07-13 13:32:22 -0300

    feat (trip-datails):  add creat ativity modal

--- src/pages/trip-detils/index.tsx
+++ src/pages/trip-detils/index.tsx
@@ -6,11 +6,23 @@ import {
   MapPin,
   Plus,
   Settings2,
+  Tag,
   UserCog,
+  X,
 } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 export function TripDetailsPage() {
+  const [isCreatActivityModalOpen, setIsCreatActivityModalOpen] =
+    useState<boolean>(false);
+
+  function openCreatActivityModal() {
+    setIsCreatActivityModalOpen(true);
+  }
+  function closeCreatActivityModal() {
+    setIsCreatActivityModalOpen(false);
+  }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -38,7 +50,10 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="gap-2 px-5 py-2 bg-lime-300 rounded-lg text-lime-950 font-medium flex items-center hover:bg-lime-400">
+            <button
+              onClick={openCreatActivityModal}
+              className="gap-2 px-5 py-2 bg-lime-300 rounded-lg text-lime-950 font-medium flex items-center hover:bg-lime-400"
+            >
               <Plus className="size-5" />
               Cadastrar atividade
             </button>
@@ -81,7 +96,6 @@ export function TripDetailsPage() {
             </div>
           </div>
         </div>
-
         <div className="w-80 space-y-6">
           <div className="space-y-6">
             <h2 className="font-semibold text-xl">Links importantes</h2>
@@ -161,6 +175,59 @@ export function TripDetailsPage() {
             </button>
           </div>
         </div>
+
+        {/* modall */}
+        {isCreatActivityModalOpen && (
+          <div className=" fixed inset-0 bg-black/60 flex items-center justify-center">
+            <div className="w-[40rem] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <h2 className="text-lg text-zinc-400">
+                    Cadastrar atividade(s)
+                  </h2>
+                  <button type="button" onClick={closeCreatActivityModal}>
+                    <X className="size-5 text-zinc-400" />
+                  </button>
+                </div>
+                <p className="text-sm text-zinc-400">
+                  Todos os convidados podem vizualizar as atividades.
+                </p>
+              </div>
+
+              <form className="flex flex-col space-y-3">
+                <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                  <Tag className="text-zinc-400 size-5" />
+
+                  <input
+                    name="title"
+                    placeholder="Qual a atividade?"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+
+                <div className="flex items-center gap-2">
+                  <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                    <Calendar className="text-zinc-400 size-5" />
+
+                    <input
+                      type="datetime-local"
+                      name="occurs_at"
+                      placeholder="Data e horŕio da atividade"
+                      className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                    />
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className="gap-2 px-5 h-11 bg-lime-300 rounded-lg text-lime-950 font-medium flex items-center justify-center hover:bg-lime-400"
+                >
+                  Salvar atividade
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );
